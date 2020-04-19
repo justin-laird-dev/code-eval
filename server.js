5a69ca965400c85e67eb34a71ca09344efdefc8a
justin-laird-dev <justinlaird@Justins-MacBook-2.local>
2020-04-19 15:39:03 -0700

    Connect to DB and verify connection using credentials from process environment, optionaly in .env file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const server = express();
 const port = 4000;
-
 
 server.get("/json", (req, res) => {
   res.json({ message: "Hello world" });
@@ -11,3 +12,23 @@ server.listen(port, () => {
   console.log(`Server listening at ${port}`);
 });
 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,  process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
